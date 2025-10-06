e603abd64d3859cf24deeb2e638c97f7da694e72
PawanKumar-Dev <pk687376@gmail.com>
2025-10-06 06:08:21 +0000

    update

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function TestPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Test Page</h1>
-    </div>
-  );
-}
